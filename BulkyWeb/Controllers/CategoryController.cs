222bc7076b59a31aa54c89c936c4df3da193b926
Anurag Chatterjee <anuragchatterjee2@gmail.com>
2024-06-25 02:23:03 +0530

    Toastr Notification added

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -4,36 +4,106 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
-	public class CategoryController : Controller
-	{
-		private readonly ApplicationDbContext _context;
-		public CategoryController(ApplicationDbContext db)
-		{
-			_context = db;
-		}
-		public IActionResult Index()
-		{
-			List<Category> objCategoryList = _context.Categories.ToList();
-			return View(objCategoryList);
-		}
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _context = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _context.Categories.ToList();
+            return View(objCategoryList);
+        }
 
 
-		public IActionResult Create()
-		{
-			return View();
-		}
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		public IActionResult Create(Category obj)
-		{
-			if (ModelState.IsValid)
-			{
-				_context.Categories.Add(obj);
-				_context.SaveChanges();
-				return RedirectToAction("Index");
-			}
-			return View();
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(obj);
+                _context.SaveChanges();
+                TempData["success"] = "Category added successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
 
-		}
-	}
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _context.Categories.FirstOrDefault(c => c.Id == id);
+            Category? categoryFromDb1 = _context.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(obj);
+                _context.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _context.Categories.FirstOrDefault(c => c.Id == id);
+            //Category? categoryFromDb1 = _context.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(obj);
+            _context.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+
+            return RedirectToAction("Index");
+
+        }
+    }
 }
